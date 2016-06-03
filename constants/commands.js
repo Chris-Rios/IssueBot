375c601705518ed453c458b8ab63c838acf32789
Christopher Rios <rioschm@gmail.com>
2016-06-03 16:14:28 -0400

    add additional commands

--- constants/commands.js
+++ constants/commands.js
@@ -1,5 +1,7 @@
 const commands = [
-  `-allIssues <user> <repo>, Lists all issues for a given user/repo`
+  `-allIssues <user> <repo>, Lists all issues for a given user/repo`,
+  `-openIssues <user> <repo>, Lists open issues for a given user/repo`,
+  `-closedIssues <user> <repo>, Lists closed issues for a given user/repo`
 ];
 
 
