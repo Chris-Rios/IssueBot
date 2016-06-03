4a6b839a72d1bc47009de6a73dfdfe19b5b1690d
Christopher Rios <rioschm@gmail.com>
2016-06-03 14:00:39 -0400

    add command to get issues, add help command

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
+'use strict';
+
 const rtmClient = require('@slack/client').RtmClient;
 const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
-const github = require('octonode');
-const client = github.client();
 const userInfo = require('./api/issues');
 const config = require('./config/config.js');
 const greetingMessages = require('./constants/greetings.js').greetingMessages;
+const commands = require('./constants/commands.js').commands;
 const greetingResponses = require('./constants/greetings.js').greetingResponses;
+const issues = require('./api/issues.js');
 
 const botToken = config.slack.chatToken;
 
@@ -20,7 +22,7 @@ rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
 
  const response = handleMessage(message.text,message.channel);
 
-  rtm.sendMessage(`${response}`, message.channel);
+ rtm.sendMessage(`${response}`, message.channel);
 });
 
 
@@ -30,9 +32,28 @@ const handleMessage = (message, channel) => {
     return greetingResponses[Math.floor(Math.random() * greetingResponses.length)];
   }
 
-  if(message === "-h") {
-    return `Here are a list of some commands`
+  if (message === "-h") {
+    let message = `>>>Here are a list of some commands:`
+    for (const command of commands) {
+      message = `${message}\n ${command}`;
+    }
+    return message
   }
 
-  return 'I don\'t understand your response, type -h for help';
+  if (message.toLowerCase().startsWith("-allissues")) {
+    message = message.split(" ");
+    
+    issues.getAllIssues(message[1],message[2]).then((result) => {
+        for(const issue of result) {
+          rtm.sendMessage(`> #${issue.number}-${issue.title}`,channel);
+        }
+      },
+      error => {
+        rtm.sendMessage(`Sorry I was unable to get issues for you!`, channel);
+      }
+    );
+    return "Sure Thing!";
+  }
+
+  return 'I don\'t understand your message, type -h for help';
 }
